281d95505fc6b287d4316bc0b2c3531129f38f0d
Luke Parham <lappp9@gmail.com>
2013-10-25 16:19:33 -0500

    added header.css with my old styles maybe matt can look at it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024005357) do
+ActiveRecord::Schema.define(version: 20131025184827) do
 
   create_table "addresses", force: true do |t|
     t.string   "street"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20131024005357) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.string   "lastname"
   end
 
 end
