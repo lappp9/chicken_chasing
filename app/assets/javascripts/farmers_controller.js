0aa938bf9c2e4225e7449fae5671ef09c7a1a67b
Luke Parham <lappp9@gmail.com>
2014-01-06 13:12:17 -0600

    removed some alerts and comments

--- app/assets/javascripts/farmers_controller.js
+++ app/assets/javascripts/farmers_controller.js
@@ -7,16 +7,10 @@ function farmerSignupCtl($scope, $http, $location){
   $scope.farmer  = {"favorite_quotes":"", "role_models":"", "personal_philosophy":""};
 
   $scope.submitForms = function(){
-    alert("submitting...");
-    //alert(JSON.stringify($scope["user"]));
-    //submitUserData();
     submitData("user");
   }
 
   submitData = function(model, id){
-    alert("the model is " + model);
-    alert("and the model info is " + JSON.stringify($scope[model]));
-
     var modelToSubmit = {"ajax":true};
     modelToSubmit[model] = $scope[model]
     var path = '/' + model + 's';
@@ -24,14 +18,10 @@ function farmerSignupCtl($scope, $http, $location){
       modelToSubmit["user_id"] = id;
     }
 
-    alert("posting " + JSON.stringify(modelToSubmit) +  " to " + path);
 
     $http.post(path, modelToSubmit).
     success( function(response){
       if( response.id ){
-        alert("Hey a success, the response was " + JSON.stringify(response));
-        //$scope.user_errors = "No errors!";
-        //$scope.user_id = response.id;
         if(model === "user"){
           submitData("farmer", response.id)
         }
@@ -40,51 +30,13 @@ function farmerSignupCtl($scope, $http, $location){
         }
       }
       else{
-        alert(response);
         $scope.user_errors = JSON.stringify(response)
-        alert(JSON.stringify(response));
       }
     }).
     error( function(response){
       alert('bad mkay'); 
     }); 
   }
-
-  //submitUserData = function(){
-    //$http.post('/users', {"user": $scope.user, "ajax": true }).
-    //success( function(response){
-      //if( response.id ){
-        //$scope.user_errors = "No errors!";
-        //$scope.user_id = response.id;
-        //submitFarmerData($scope.user_id);
-      //}
-      //else{
-        //alert(response);
-        //$scope.user_errors = JSON.stringify(response)
-        //alert(JSON.stringify(response));
-      //}
-    //}).
-    //error( function(response){
-      //alert("error fag");
-    //});
-  //}
-
-  //submitFarmerData = function(userId){
-    //$http.post('/farmers', {"farmer": $scope.farmer, "ajax": true, "user_id": userId }).
-    //success( function(response){
-      //if( response.id ){
-        //$scope.farmer_errors = "No errors!";
-        //alert(window.location.href);
-        ////$location.path("/farmers/" + response.id + "" );
-      //}
-      //else{
-        //$scope.farmer_errors = JSON.stringify(response)
-      //}
-    //}).
-    //error( function(response){
-      //alert("error fag");
-    //});
-  //}
 
 }
 
