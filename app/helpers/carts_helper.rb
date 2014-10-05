9231953a83c959dde065e6287078aac40e13b7bf
Jim Lien <jamesslien@gmail.com>
2014-10-05 15:49:59 -0500

    Fixed bug in cart that didnt show full cart total.

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,9 +2,7 @@ module CartsHelper
 
   def cart_total_for(product_hash)
     total = 0
-    product_hash.keys.each do |p|
-      total += p.price
-    end
+    product_hash.each  { |k,v| total += k.price * v }
     total
   end
 
